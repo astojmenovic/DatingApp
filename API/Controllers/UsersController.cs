92f507c6e194d4adea4864c26317820fb301e91a
astojmenovic <aleksandar.stojmenovic9@gmail.com>
2020-12-29 02:41:44 +0100

    Identity and role management

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -39,7 +39,6 @@ namespace API.Controllers
             return Ok(users);
         }
 
-
         [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
